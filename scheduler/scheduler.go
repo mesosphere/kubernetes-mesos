aa771607077f32fdeb80c355931fca6ecf3eb165
Yifan Gu <guyifan1121@gmail.com>
2014-07-09 22:55:25 -0700

    Done LaunchTask.

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,15 +1,17 @@
 package scheduler
 
 import (
-	"container/list"
+	"container/ring"
 	"fmt"
 	"sync"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
 	kubernetes "github.com/GoogleCloudPlatform/kubernetes/pkg/scheduler"
+	"github.com/mesosphere/kubernetes-mesos/3rdparty/code.google.com/p/goprotobuf/proto"
 	log "github.com/mesosphere/kubernetes-mesos/3rdparty/github.com/golang/glog"
 	"github.com/mesosphere/kubernetes-mesos/3rdparty/github.com/mesosphere/mesos-go/mesos"
+	"gopkg.in/v1/yaml"
 )
 
 var errSchedulerTimeout = fmt.Errorf("Schedule time out")
@@ -22,20 +24,46 @@ const defaultFinishedTasksSize = 1024
 //
 // The Schedule function takes a group of slaves (each contains offers
 // from that slave), and a group of pods.
-// To schedule a pod, just fill the 'SelectedMachine' field of
-// the PodTask. See the FIFOScheduleFunc for example.
-type PodScheduleFunc func(slaves map[string]*Slave, tasks *list.List)
+//
+// After deciding which slave to schedule the pod, it fills the task info and the
+//'SelectedMachine' channel  with the host name of the slave.
+// See the FIFOScheduleFunc for example.
+type PodScheduleFunc func(slaves map[string]*Slave, tasks map[string]*PodTask) []*PodTask
 
 // A struct describes a pod task.
 type PodTask struct {
+	ID              string
 	Pod             *api.Pod
 	Machines        []string
 	SelectedMachine chan string
 	TaskInfo        *mesos.TaskInfo
+	OfferIds        []string
+}
+
+// Fill the TaskInfo in the PodTask, should be called in PodScheduleFunc.
+func (t *PodTask) FillTaskInfo(slaveId string, offers ...*mesos.Offer) {
+	for _, offer := range offers {
+		t.OfferIds = append(t.OfferIds, offer.GetId().GetValue())
+	}
+
+	var err error
+	t.TaskInfo = &mesos.TaskInfo{
+		Name:    proto.String("PodTask"),
+		TaskId:  &mesos.TaskID{Value: proto.String(t.ID)},
+		SlaveId: &mesos.SlaveID{Value: proto.String(slaveId)},
+		// TODO(yifan): ExecutorInfo
+	}
+	// TODO(yifan): Get offer from the resources.
+	t.TaskInfo.Resources = offers[0].GetResources()
+	t.TaskInfo.Data, err = yaml.Marshal(&t.Pod.DesiredState.Manifest)
+	if err != nil {
+		log.Warningf("Failed to marshal the manifest")
+	}
 }
 
 func newPodTask(pod *api.Pod, machines []string) *PodTask {
 	task := &PodTask{
+		ID:              pod.JSONBase.ID,
 		Pod:             pod,
 		Machines:        make([]string, len(machines)),
 		SelectedMachine: make(chan string, 1),
@@ -47,15 +75,16 @@ func newPodTask(pod *api.Pod, machines []string) *PodTask {
 
 // A struct that describes the slave.
 type Slave struct {
-	Offers map[string]*mesos.Offer
-	Tasks  map[string]*PodTask
-	State  bool // connected or disconnected.
+	HostName string
+	Offers   map[string]*mesos.Offer
+	tasks    map[string]*PodTask
 }
 
-func newSlave() *Slave {
+func newSlave(hostName string) *Slave {
 	return &Slave{
-		Offers: make(map[string]*mesos.Offer),
-		Tasks:  make(map[string]*PodTask),
+		HostName: hostName,
+		Offers:   make(map[string]*mesos.Offer),
+		tasks:    make(map[string]*PodTask),
 	}
 }
 
@@ -81,10 +110,13 @@ type KubernetesScheduler struct {
 	// SlaveID => slave.
 	slaves map[string]*Slave
 
+	// Slave's hostname => slaveID
+	slaveIDs map[string]string
+
 	// Fields for task information book keeping.
-	pendingTasks  *list.List
+	pendingTasks  map[string]*PodTask
 	runningTasks  map[string]*PodTask
-	finishedTasks []*PodTask
+	finishedTasks *ring.Ring
 
 	// The function that does scheduling.
 	scheduleFunc PodScheduleFunc
@@ -100,19 +132,12 @@ func New(driver mesos.SchedulerDriver, scheduleFunc PodScheduleFunc) *Kubernetes
 		false,
 		make(map[string]*mesos.Offer),
 		make(map[string]*Slave),
-		list.New(),
+		make(map[string]string),
 		make(map[string]*PodTask),
-		make([]*PodTask, defaultFinishedTasksSize),
+		make(map[string]*PodTask),
+		ring.New(defaultFinishedTasksSize),
 		scheduleFunc,
 	}
-}
-
-// Add a new task to the queue.
-func (k *KubernetesScheduler) addTask(task *PodTask) {
-	k.Lock()
-	defer k.Unlock()
-	k.pendingTasks.PushBack(task)
-	k.scheduleFunc(k.slaves, k.pendingTasks)
 }
 
 // Registered is called when the scheduler registered with the master successfully.
@@ -145,11 +170,22 @@ func (k *KubernetesScheduler) ResourceOffers(driver mesos.SchedulerDriver, offer
 	k.Lock()
 	defer k.Unlock()
 
-	// Add offers to the pool.
+	// Record the offers in the global offer map as well as each slave's offer map.
 	for _, offer := range offers {
-		k.offers[offer.GetId().GetValue()] = offer
+		offerId := offer.GetId().GetValue()
+		slaveId := offer.GetSlaveId().GetValue()
+
+		slave, exists := k.slaves[slaveId]
+		if !exists {
+			k.slaves[slaveId] = newSlave(offer.GetHostname())
+			slave = k.slaves[slaveId]
+		}
+		slave.Offers[offerId] = offer
+		k.offers[offerId] = offer
+		k.slaveIDs[slave.HostName] = slaveId
+		// TODO(yifan): Aggregate resources.
 	}
-	k.scheduleFunc(k.slaves, k.pendingTasks)
+	k.doSchedule()
 }
 
 // OfferRescinded is called when the resources are recinded from the scheduler.
@@ -158,13 +194,107 @@ func (k *KubernetesScheduler) OfferRescinded(driver mesos.SchedulerDriver, offer
 
 	k.Lock()
 	defer k.Unlock()
+
+	slaveId := k.offers[offerId.GetValue()].GetSlaveId().GetValue()
 	delete(k.offers, offerId.GetValue())
+	delete(k.slaves[slaveId].Offers, offerId.GetValue())
 }
 
 // StatusUpdate is called when a status update message is sent to the scheduler.
 func (k *KubernetesScheduler) StatusUpdate(driver mesos.SchedulerDriver, taskStatus *mesos.TaskStatus) {
 	log.Infof("Received status update %v\n", taskStatus)
 	// TODO(yifan): Check status.
+	k.Lock()
+	defer k.Unlock()
+
+	switch taskStatus.GetState() {
+	case mesos.TaskState_TASK_STAGING:
+		k.handleTaskStaging(taskStatus)
+	case mesos.TaskState_TASK_STARTING:
+		k.handleTaskStarting(taskStatus)
+	case mesos.TaskState_TASK_RUNNING:
+		k.handleTaskRunning(taskStatus)
+	case mesos.TaskState_TASK_FINISHED:
+		k.handleTaskFinished(taskStatus)
+	case mesos.TaskState_TASK_FAILED:
+		k.handleTaskFailed(taskStatus)
+	case mesos.TaskState_TASK_KILLED:
+		k.handleTaskKilled(taskStatus)
+	case mesos.TaskState_TASK_LOST:
+		k.handleTaskLost(taskStatus)
+	}
+}
+
+func (k *KubernetesScheduler) handleTaskStaging(taskStatus *mesos.TaskStatus) {
+	log.Errorf("Not implemented")
+}
+
+func (k *KubernetesScheduler) handleTaskStarting(taskStatus *mesos.TaskStatus) {
+	log.Errorf("Not implemented")
+}
+
+func (k *KubernetesScheduler) handleTaskRunning(taskStatus *mesos.TaskStatus) {
+	taskId, slaveId := taskStatus.GetTaskId().GetValue(), taskStatus.GetSlaveId().GetValue()
+	slave, exists := k.slaves[slaveId]
+	if !exists {
+		log.Warningf("Ignore status TASK_RUNNING because the slave does not exist\n")
+		return
+	}
+	task, exists := k.pendingTasks[taskId]
+	if !exists {
+		log.Warningf("Ignore status TASK_RUNNING because the the task is discarded")
+		return
+	}
+	if _, exists = k.runningTasks[taskId]; exists {
+		log.Warningf("Ignore status TASK_RUNNING because the the task is already running")
+		return
+	}
+	if containsTask(k.finishedTasks, taskId) {
+		log.Warningf("Ignore status TASK_RUNNING because the the task is already finished")
+		return
+	}
+
+	k.runningTasks[taskId] = task
+	slave.tasks[taskId] = task
+	delete(k.pendingTasks, taskId)
+	fmt.Println("Task is running %v", taskId)
+}
+
+func (k *KubernetesScheduler) handleTaskFinished(taskStatus *mesos.TaskStatus) {
+	taskId, slaveId := taskStatus.GetTaskId().GetValue(), taskStatus.GetSlaveId().GetValue()
+	slave, exists := k.slaves[slaveId]
+	if !exists {
+		log.Warningf("Ignore status TASK_FINISHED because the slave does not exist\n")
+		return
+	}
+	if _, exists := k.pendingTasks[taskId]; exists {
+		panic("Pending task finished, this couldn't happen")
+	}
+	if _, exists := k.runningTasks[taskId]; exists {
+		log.Warningf("Ignore status TASK_FINISHED because the the task is not running")
+		return
+	}
+	if containsTask(k.finishedTasks, taskId) {
+		log.Warningf("Ignore status TASK_FINISHED because the the task is already finished")
+		return
+	}
+
+	k.finishedTasks.Next().Value = taskId
+	delete(k.runningTasks, taskId)
+	delete(slave.tasks, taskId)
+	fmt.Println("Task is finished %v", taskId)
+}
+
+func (k *KubernetesScheduler) handleTaskFailed(taskStatus *mesos.TaskStatus) {
+	log.Errorf("Not implemented")
+}
+
+func (k *KubernetesScheduler) handleTaskKilled(taskStatus *mesos.TaskStatus) {
+	log.Errorf("Not implemented")
+}
+
+func (k *KubernetesScheduler) handleTaskLost(taskStatus *mesos.TaskStatus) {
+	log.Errorf("Not implemented")
 }
 
 // FrameworkMessage is called when the scheduler receives a message from the executor.
@@ -192,16 +322,35 @@ func (k *KubernetesScheduler) Error(driver mesos.SchedulerDriver, message string
 }
 
 // Schedule implements the Scheduler interface of the Kubernetes.
-func (k *KubernetesScheduler) Schedule(pod api.Pod, minionLister kubernetes.MinionLister) (selectedMachine string, err error) {
+// It returns the selectedMachine's name and error (if there's any).
+func (k *KubernetesScheduler) Schedule(pod api.Pod, minionLister kubernetes.MinionLister) (string, error) {
 	machineLists, err := minionLister.List()
 	if err != nil {
 		log.Warningf("minionLister.List() error %v\n", err)
 		return "", err
 	}
-
 	task := newPodTask(&pod, machineLists)
-	k.addTask(task)
+
+	k.Lock()
+	k.pendingTasks[task.ID] = task
+	k.doSchedule()
+	k.Unlock()
+
 	return <-task.SelectedMachine, nil
+}
+
+// Call ScheduleFunc and substract some resources.
+func (k *KubernetesScheduler) doSchedule() {
+	if tasks := k.scheduleFunc(k.slaves, k.pendingTasks); tasks != nil {
+		// Substract offers.
+		for _, task := range tasks {
+			for _, offerId := range task.OfferIds {
+				slaveId := task.TaskInfo.GetSlaveId().GetValue()
+				delete(k.offers, offerId)
+				delete(k.slaves[slaveId].Offers, offerId)
+			}
+		}
+	}
 }
 
 // ListPods obtains a list of pods that match selector.
@@ -211,18 +360,33 @@ func (k *KubernetesScheduler) ListPods(selector labels.Selector) ([]api.Pod, err
 
 // Get a specific pod.
 func (k *KubernetesScheduler) GetPod(podID string) (*api.Pod, error) {
-	// TODO(yifan): Send a message to slave/executor to get the pod status.
-	// Need to implement this to let the PodRegistryStorage know that
-	// the pod is running.
-	//
-	// Consider to read from a channel.
-	return nil, fmt.Errorf("Not implemented")
+	k.RLock()
+	defer k.RUnlock()
+
+	// Note that podID is also the taskId.
+	if _, exists := k.pendingTasks[podID]; exists {
+		return nil, fmt.Errorf("Pod is still pending")
+	}
+	if containsTask(k.finishedTasks, podID) {
+		return nil, fmt.Errorf("Pod is finished")
+	}
+	if task, exists := k.runningTasks[podID]; exists {
+		return task.Pod, nil
+	}
+	return nil, fmt.Errorf("Unknown Pod %v", podID)
 }
 
 // Create a pod based on a specification, schedule it onto a specific machine.
 func (k *KubernetesScheduler) CreatePod(machine string, pod api.Pod) error {
-	// TODO(yifan): Call launchTask.
-	return fmt.Errorf("Not implemented")
+	taskId := pod.JSONBase.ID
+	task, exists := k.pendingTasks[taskId]
+	if !exists {
+		return fmt.Errorf("Pod Task does not exist %v\n", taskId)
+	}
+
+	// TODO(yifan): By this time, there is a chance that the slave is disconnected.
+	offerId := &mesos.OfferID{Value: proto.String(task.OfferIds[0])}
+	return k.driver.LaunchTasks(offerId, []*mesos.TaskInfo{task.TaskInfo}, nil)
 }
 
 // Update an existing pod.
@@ -237,17 +401,39 @@ func (k *KubernetesScheduler) DeletePod(podID string) error {
 	return fmt.Errorf("Not implemented")
 }
 
-// A FIFO scheduler.
-func FIFOScheduleFunc(slaves map[string]*Slave, tasks *list.List) {
-	for _, slave := range slaves {
-		for task := tasks.Front(); task != nil; task = task.Next() {
-			_ = slave
-			// TODO(yifan):
-			// if resources is ok.
-			// task.SelectedMachine <- slave's hostname
-
-			// Just schedule one pod for now.
-			break
+// A FCFS scheduler.
+func FCFSScheduleFunc(slaves map[string]*Slave, tasks map[string]*PodTask) []*PodTask {
+	for _, task := range tasks {
+		for slaveId, slave := range slaves {
+			if !containsHostName(task.Machines, slave.HostName) {
+				continue
+			}
+			for _, offer := range slave.Offers {
+				// Fill the task info.
+				task.FillTaskInfo(slaveId, offer)
+				task.SelectedMachine <- slave.HostName
+				// Just schedule one task for now.
+				return []*PodTask{task}
+			}
 		}
 	}
+	return nil
+}
+
+func containsHostName(machines []string, machine string) bool {
+	for _, m := range machines {
+		if m == machine {
+			return true
+		}
+	}
+	return false
+}
+
+func containsTask(finishedTasks *ring.Ring, taskId string) bool {
+	for i := 0; i < finishedTasks.Len(); i++ {
+		if finishedTasks.Next().Value.(string) == taskId {
+			return true
+		}
+	}
+	return false
 }
