a96e101778bd7180ed6def94678ac213dae40609
James DeFelice <james.defelice@gmail.com>
2014-11-07 08:10:29 -0500

    allow arbitrary shell commands to run in the container

--- hack/dockerbuild/run.sh
+++ hack/dockerbuild/run.sh
@@ -5,11 +5,11 @@ set -e
 set -o pipefail
 set -v
 
-test ${#} -eq 0 && MAKE_ARGS=( bootstrap install ) || MAKE_ARGS=( "${@}" )
+test ${#} -eq 0 && CMD=( make bootstrap install ) || CMD=( "${@}" )
 GOPKG=github.com/mesosphere/kubernetes-mesos
 
 mkdir -pv /pkg/src/${GOPKG} && cd /pkg/src/${GOPKG}
 git clone https://${GOPKG}.git .
 
 test "x${GIT_BRANCH}" = "x" || git checkout "${GIT_BRANCH}"
-make "${MAKE_ARGS[@]}"
+exec "${CMD[@]}"
