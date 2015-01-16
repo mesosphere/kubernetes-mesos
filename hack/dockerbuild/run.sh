b7d9c43d470083bc9001c738527ba58ac5ade739
James DeFelice <james.defelice@gmail.com>
2015-01-16 05:40:59 +0000

    fix bug with bad symlink created upon reattaching to stopped dockerbuilder env

--- hack/dockerbuild/run.sh
+++ hack/dockerbuild/run.sh
@@ -3,16 +3,23 @@ echo Running with args "${@}"
 
 set -e
 set -o pipefail
-set -v
-
-test ${#} -eq 0 && CMD=( make bootstrap install ) || CMD=( "${@}" )
+set -vx
 
 GOPKG=github.com/mesosphere/kubernetes-mesos
-test -d $SNAP && {
-  parent=$(dirname /pkg/src/${GOPKG})
-  mkdir -pv $parent
-  ln -sv $SNAP $parent/$(basename $GOPKG)
-  cd /pkg/src/${GOPKG}
+
+test ${#} -eq 0 && CMD=( make bootstrap install ) || CMD=( "${@}" )
+test -n "$GOPATH" || GOPATH=/pkg
+
+# gopath is first directory in GOPATH : separated list
+gopath="${GOPATH%%:*}"
+pkg="${gopath}/src/${GOPKG}"
+
+if [ -d $SNAP ]; then
+  test ! -L "${pkg}" || /bin/rm -vf "${pkg}"  # remove any existing link
+  parent=$(dirname "${pkg}")
+  mkdir -pv "$parent"
+  ln -sv $SNAP "$parent/$(basename $GOPKG)"
+  cd "${pkg}"
   if [ "x${GIT_BRANCH}" != "x" ]; then
     if test -d '.git'; then
       git checkout "${GIT_BRANCH}"
@@ -21,11 +28,11 @@ test -d $SNAP && {
       exit 1
     fi
   fi
-} || {
-  mkdir -pv /pkg/src/${GOPKG}
-  cd /pkg/src/${GOPKG}
+else
+  mkdir -pv "${pkg}"
+  cd "${pkg}"
   git clone ${GIT_REPO:-https://${GOPKG}.git} .
   test "x${GIT_BRANCH}" = "x" || git checkout "${GIT_BRANCH}"
-}
+fi
 
 exec "${CMD[@]}"
