1e0bfcfc9c06c2badcaff5410e49a60a268a1c51
James DeFelice <james.defelice@gmail.com>
2015-02-25 18:54:00 +0000

    update to use kubectl

--- hack/testing/resizingFrontend.sh
+++ hack/testing/resizingFrontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-test -n "$KUBECFG" || KUBECFG=./bin/kubecfg
+test -n "$KUBECFG" || KUBECFG=./bin/kubectl
 test -x "$KUBECFG" || {
     echo "error: missing kubecfg executable at $KUBECFG" >&2
     exit 1
@@ -12,6 +12,6 @@ trap ontrap INT HUP TERM
 echo "starting resizing loop for frontendController"
 while true; do
     x=$((RANDOM % 20))
-    "$KUBECFG" resize frontendController $(( x + 1 )) > /dev/null
+    "$KUBECFG" resize --replicas=$(( x + 1 )) rc frontend-controller > /dev/null
     sleep $(( (RANDOM % 15) + 15 ))
 done
