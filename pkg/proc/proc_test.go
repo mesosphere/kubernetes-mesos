8df626d2d7659688416c11550a330338ea76ea92
James DeFelice <james.defelice@gmail.com>
2015-03-30 22:08:28 +0000

    test FIFO property of process action scheduler

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -1,6 +1,7 @@
 package proc
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -87,6 +88,51 @@ func TestProc_singleAction(t *testing.T) {
 	case <-called:
 	case <-time.After(1 * time.Second):
 		t.Fatalf("timed out waiting for deferred action to be invoked")
+	}
+
+	p.End()
+
+	select {
+	case <-p.Done():
+	case <-time.After(2 * time.Second):
+		t.Fatalf("timed out waiting for process death")
+	}
+}
+
+func TestProc_multiAction(t *testing.T) {
+	p := New()
+	p.Begin()
+	const COUNT = 10
+	var called sync.WaitGroup
+	called.Add(COUNT)
+
+	// test FIFO property
+	next := 0
+	for i := 0; i < COUNT; i++ {
+		log.Infof("do'ing deferred action %d", i)
+		idx := i
+		err := p.Do(func() {
+			defer called.Done()
+			log.Infof("deferred action invoked")
+			if next != idx {
+				t.Fatalf("expected index %d instead of %d", idx, next)
+			}
+			next++
+		})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	}
+
+	ch := make(chan struct{})
+	go func() {
+		defer close(ch)
+		called.Wait()
+	}()
+	select {
+	case <-ch:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out waiting for deferred actions to be invoked")
 	}
 
 	p.End()
