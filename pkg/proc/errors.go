8e5ea1caf3d515b4cee0696ef727985f102cbb72
James DeFelice <james.defelice@gmail.com>
2015-03-30 18:45:38 +0000

    removed unused error code

--- pkg/proc/errors.go
+++ pkg/proc/errors.go
@@ -5,15 +5,10 @@ import (
 )
 
 var (
-	errActionNotAllowed      = errors.New("action is not permitted to run")
 	errProcessTerminated     = errors.New("cannot execute action because process has terminated")
 	errIllegalState          = errors.New("illegal state, cannot execute action")
 	errActionScheduleTimeout = errors.New("timed out attempting to schedule action for deferred execution")
 )
-
-func IsActionNotAllowed(err error) bool {
-	return err == errActionNotAllowed
-}
 
 func IsProcessTerminated(err error) bool {
 	return err == errProcessTerminated
