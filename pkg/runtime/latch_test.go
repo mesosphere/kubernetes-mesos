c5f420e6a668932487c16690af11c1c8f40fd90f
James DeFelice <james.defelice@gmail.com>
2015-04-09 20:01:08 +0000

    added latch test

---
+++ pkg/runtime/latch_test.go
@@ -0,0 +1,43 @@
+package runtime
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func Test_LatchAcquireBasic(t *testing.T) {
+	var x Latch
+	if !x.Acquire() {
+		t.Fatalf("expected first acquire to succeed")
+	}
+	if x.Acquire() {
+		t.Fatalf("expected second acquire to fail")
+	}
+	if x.Acquire() {
+		t.Fatalf("expected third acquire to fail")
+	}
+}
+
+func Test_LatchAcquireConcurrent(t *testing.T) {
+	var x Latch
+	const NUM = 10
+	ch := make(chan struct{})
+	var success int32
+	var wg sync.WaitGroup
+	wg.Add(NUM)
+	for i := 0; i < NUM; i++ {
+		go func() {
+			defer wg.Done()
+			<-ch
+			if x.Acquire() {
+				atomic.AddInt32(&success, 1)
+			}
+		}()
+	}
+	close(ch)
+	wg.Wait()
+	if success != 1 {
+		t.Fatalf("expected single acquire to succeed instead of %d", success)
+	}
+}
