ddc4c14e12b6e0da1e60b025aa242e7ab876b5df
James DeFelice <james.defelice@gmail.com>
2015-04-15 18:26:39 +0000

    reduce default cluster size and attempt to avoid k8s madness

--- hack/deploy/etcd/new-etcd.sh
+++ hack/deploy/etcd/new-etcd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-test -n "$CLUSTER_SIZE" || CLUSTER_SIZE=3
+test -n "$CLUSTER_SIZE" || CLUSTER_SIZE=1
 d=$(curl -f https://discovery.etcd.io/new?size=$CLUSTER_SIZE) || {
 	echo "ERROR: failed to create discovery URL"
 	exit 1
 }
-cat $(dirname "$0")/etcd.json.template | sed -e "s~ETCD_DISCOVERY_VALUE~$d~g"
+cat $(dirname "$0")/etcd.json.template | sed \
+	-e "s~ETCD_DISCOVERY_VALUE~$d~g" \
+	-e "s~CLUSTER_SIZE_VALUE~$CLUSTER_SIZE~g"
