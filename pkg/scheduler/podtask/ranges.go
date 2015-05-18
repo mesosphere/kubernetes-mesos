deb9ff948cd813efa3ec3a8c72db1f1fea8f46f4
Tomás Senart <tomas@mesosphere.io>
2015-05-18 21:33:36 +0200

    Implement robust Ranges data structure

---
+++ pkg/scheduler/podtask/ranges.go
@@ -0,0 +1,104 @@
+package podtask
+
+import (
+	"sort"
+
+	"github.com/gogo/protobuf/proto"
+	mesos "github.com/mesos/mesos-go/mesosproto"
+)
+
+// Range is an all-inclusive range defined by a lower and upper bound.
+type Range [2]uint64
+
+// Ranges represents a list of Ranges.
+type Ranges []Range
+
+// NewPortRanges returns Ranges from the "ports" resource in the
+// given *mesos.Offer. If that resource isn't provided, nil will be returned.
+//
+// The returned Ranges are sorted and have all overlapping ranges merged from
+// left to right. e.g. [[0, 5], [4, 3], [10, 7]] -> [[0, 5], [7, 10]]
+func NewPortRanges(o *mesos.Offer) Ranges {
+	if o == nil {
+		return nil
+	}
+
+	var r *mesos.Resource
+	for i := range o.Resources {
+		if o.Resources[i].GetName() == "ports" {
+			r = o.Resources[i]
+			break
+		}
+	}
+
+	if r == nil {
+		return nil
+	}
+
+	offered := r.GetRanges().GetRange()
+	rs := make(Ranges, len(offered))
+	for i, r := range offered {
+		if lo, hi := r.GetBegin(), r.GetEnd(); lo <= hi {
+			rs[i][0], rs[i][1] = lo, hi
+		} else {
+			rs[i][0], rs[i][1] = hi, lo
+		}
+	}
+	sort.Sort(rs)
+
+	return rs.Squash()
+}
+
+// These three methods implement sort.Interface
+func (rs Ranges) Len() int           { return len(rs) }
+func (rs Ranges) Less(i, j int) bool { return rs[i][0] < rs[j][0] && rs[i][1] < rs[j][1] }
+func (rs Ranges) Swap(i, j int)      { rs[i], rs[j] = rs[j], rs[i] }
+
+// Squash merges overlapping and continuous Ranges. It assumes they're pre-sorted.
+func (rs Ranges) Squash() Ranges {
+	if len(rs) < 2 {
+		return rs
+	}
+	squashed := Ranges{rs[0]}
+	for i := 1; i < len(rs); i++ {
+		switch top := squashed[len(squashed)-1]; {
+		case 1+top[1] < rs[i][0]: // no overlap nor continuity: push
+			squashed = append(squashed, rs[i])
+		case 1+top[1] <= rs[i][1]: // overlap or continuity: squash
+			squashed[len(squashed)-1][1] = rs[i][1]
+		}
+	}
+	return squashed
+}
+
+// Find performs a binary search for n returning the index of the Range it was
+// found at or -1 if not found.
+func (rs Ranges) Find(n uint64) int {
+	for lo, hi := 0, len(rs)-1; lo <= hi; {
+		switch m := lo + (hi-lo)/2; {
+		case n < rs[m][0]:
+			hi = m - 1
+		case n > rs[m][1]:
+			lo = m + 1
+		default:
+			return m
+		}
+	}
+	return -1
+}
+
+// resource returns a *mesos.Resource with the given name and Ranges.
+func (rs Ranges) resource(name string) *mesos.Resource {
+	vr := make([]*mesos.Value_Range, len(rs))
+	for i := range rs {
+		vr[i] = &mesos.Value_Range{
+			Begin: proto.Uint64(rs[i][0]),
+			End:   proto.Uint64(rs[i][1]),
+		}
+	}
+	return &mesos.Resource{
+		Name:   proto.String(name),
+		Type:   mesos.Value_RANGES.Enum(),
+		Ranges: &mesos.Value_Ranges{Range: vr},
+	}
+}
---
+++ pkg/scheduler/podtask/ranges_test.go
@@ -0,0 +1,72 @@
+package podtask
+
+import (
+	"reflect"
+	"testing"
+
+	mesos "github.com/mesos/mesos-go/mesosproto"
+)
+
+func TestNewPortRanges(t *testing.T) {
+	for i, tt := range []struct {
+		Ranges
+		want Ranges
+	}{
+		{Ranges{{2, 0}, {3, 10}}, Ranges{{0, 10}}},
+		{Ranges{{0, 2}, {4, 10}}, Ranges{{0, 2}, {4, 10}}},
+		{Ranges{{10, 0}}, Ranges{{0, 10}}},
+		{Ranges{}, Ranges{}},
+		{nil, Ranges{}},
+	} {
+		offer := &mesos.Offer{Resources: []*mesos.Resource{tt.resource("ports")}}
+		if got := NewPortRanges(offer); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("test #%d, %v: got: %v, want: %v", i, tt.Ranges, got, tt.want)
+		}
+	}
+}
+
+func TestRanges_Squash(t *testing.T) {
+	for i, tt := range []struct {
+		Ranges
+		want Ranges
+	}{
+		{Ranges{}, Ranges{}},
+		{Ranges{{0, 1}}, Ranges{{0, 1}}},
+		{Ranges{{0, 2}, {1, 5}, {2, 10}}, Ranges{{0, 10}}},
+		{Ranges{{0, 2}, {2, 5}, {5, 10}}, Ranges{{0, 10}}},
+		{Ranges{{0, 2}, {3, 5}, {6, 10}}, Ranges{{0, 10}}},
+		{Ranges{{0, 2}, {4, 11}, {6, 10}}, Ranges{{0, 2}, {4, 11}}},
+		{Ranges{{0, 2}, {4, 5}, {6, 7}, {8, 10}}, Ranges{{0, 2}, {4, 10}}},
+		{Ranges{{0, 2}, {4, 6}, {8, 10}}, Ranges{{0, 2}, {4, 6}, {8, 10}}},
+		{Ranges{{0, 1}, {2, 5}, {4, 8}}, Ranges{{0, 8}}},
+	} {
+		if got := tt.Squash(); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("test #%d, Squash(%v): got: %v, want: %v", i, tt.Ranges, got, tt.want)
+		}
+	}
+}
+
+func TestRanges_Find(t *testing.T) {
+	for i, tt := range []struct {
+		Ranges
+		n    uint64
+		want int
+	}{
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 0, 0},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 1, 0},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 2, 0},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 3, 1},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 4, 1},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 5, 1},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 6, -1},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 7, 2},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 8, 2},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 9, 2},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 10, 2},
+		{Ranges{{0, 2}, {3, 5}, {7, 10}}, 11, -1},
+	} {
+		if got := tt.Find(tt.n); got != tt.want {
+			t.Errorf("test #%d: Find(%v, %v): got: %v, want: %v", i, tt.Ranges, tt.n, got, tt.want)
+		}
+	}
+}
