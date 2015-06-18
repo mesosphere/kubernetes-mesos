3a2a78575a10474c0f7da7136a83d3ecff19229c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2015-06-18 11:23:28 +0200

    Add resolveip to k8s-mesos-slave container

--- docker/mesos-slave/build.sh
+++ docker/mesos-slave/build.sh
@@ -5,6 +5,9 @@ set -ex
 script_dir=$(cd $(dirname $0) && pwd -P)
 cd ${script_dir}
 
+cp ../bin/resolveip .
+trap "rm resolveip" EXIT
+
 # build docker image
 echo "Building kubernetes-mesos-slave docker image"
 docker build -t mesosphere/kubernetes-mesos-slave .
