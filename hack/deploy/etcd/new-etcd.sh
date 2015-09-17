c6a2375c1d213c58968142cc3aba0c68673a62b2
Karl Isenberg <karl@mesosphere.io>
2015-09-17 14:05:11 -0700

    Operation: Ion Cannon (nuke all the things!)

--- hack/deploy/etcd/new-etcd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-test -n "$CLUSTER_SIZE" || CLUSTER_SIZE=1
-d=$(curl -f https://discovery.etcd.io/new?size=$CLUSTER_SIZE) || {
-	echo "ERROR: failed to create discovery URL"
-	exit 1
-}
-cat $(dirname "$0")/etcd.json.template | sed \
-	-e "s~ETCD_DISCOVERY_VALUE~$d~g" \
-	-e "s~CLUSTER_SIZE_VALUE~$CLUSTER_SIZE~g"
